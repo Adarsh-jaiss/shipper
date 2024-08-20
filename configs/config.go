0a8ee1eee98e35cf94a1104e56ac17fd58c2f203
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-08-21 01:43:15 +0530

    removed unused classes from the configs
    Signed-off-by: adarsh-jaiss <its.adarshjaiss@gmail.com>

--- configs/config.go
+++ configs/config.go
@@ -1,32 +1,17 @@
 package configs
 
-type PushSecretConfig struct {
+type Build struct {
 	RegistryServer   string `json:"registry_server"`
 	RegistryUser     string `json:"registry_user"`
 	RegistryPassword string `json:"registry_password"`
 	RegistryEmail    string `json:"registry_email"`
-}
-
-type BuildConfig struct {
-	BuildName        string        `json:"buildName"`
-	SourceType       SourceType    `json:"sourceType"`
-	BuildRunDeletion bool          `json:"buildRunDeletion"`
-	GithubURl        string        `json:"githubUrl"`
-	BuildStrategy    BuildStrategy `json:"buildStrategy"`
-}
-
-type Build struct {
-	RegistryServer   string     `json:"registry_server"`
-	RegistryUser     string     `json:"registry_user"`
-	RegistryPassword string     `json:"registry_password"`
-	RegistryEmail    string     `json:"registry_email"`
-	BuildName        string     `json:"buildName"`
-	SourceType       SourceType `json:"sourceType"`
-	BuildRunDeletion bool       `json:"buildRunDeletion"`
-	GithubURl        string     `json:"githubUrl"`
-	BuildStrategy    string     `json:"buildStrategy"`
-	ImageName        string     `json:"imageName"`
-	Timeout          string     `json:"timeout"`
+	BuildName        string `json:"buildName"`
+	SourceType       string `json:"sourceType"`
+	BuildRunDeletion bool   `json:"buildRunDeletion"`
+	GithubURl        string `json:"githubUrl"`
+	BuildStrategy    string `json:"buildStrategy"`
+	ImageName        string `json:"imageName"`
+	Timeout          string `json:"timeout"`
 }
 
 type SourceType int
@@ -47,9 +32,3 @@ const (
 	Ko
 	SourceToImage
 )
-
-type BuildRunConfig struct {
-	BuildName string `json:"buildName"`
-	ImageName string `json:"imageName"`
-	Timeout   string `json:"timeout"`
-}
